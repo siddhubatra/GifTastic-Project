2055601053713a41ff7546578875dad3a04046ec
thesidbatra <thesidbatra@gmail.com>
2019-06-23 20:51:36 -0400

    added submit button functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,6 @@ var topics;
 
 topics = ["friends", "new girl", "seinfeld", "one tree hill", "rick and morty", "breaking bad", "parks and recreation", "family guy", "south park", "the simpsons"];
 
-for (var i = 0; i < topics.length; i++) {
-    var button = $("<button>").text(topics[i]);
-    button.addClass("tvShow-button btn btn-primary");
-    button.attr("data-name", topics[i]);
-    button.css("margin", "10px");
-    $(".buttonRow").append(button);
-}
-
 function displayGifTable() {
     var tvShow = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvShow + "&api_key=sijIZlRHCEAIYIwrlTJZVWbdlc495geu&limit=10";
@@ -32,6 +24,26 @@ function displayGifTable() {
     });
 }
 
+function renderButtons() {
+    $(".buttonRow").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var button = $("<button>").text(topics[i]);
+        button.addClass("tvShow-button btn btn-primary");
+        button.attr("data-name", topics[i]);
+        button.css("margin", "10px");
+        $(".buttonRow").append(button);
+    }
+}
 
+$("#add-tvShow").on("click", function (event) {
+    event.preventDefault();
+    var tvShow = $("#tvShow-input").val().trim();
+    topics.push(tvShow);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});
 
 $(document).on("click", ".tvShow-button", displayGifTable);
+
+renderButtons();
